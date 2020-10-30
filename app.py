b7839c096eae1c0e2e5191c13a7728a60c4289e6
Yug Thapar <yugthapar37@gmail.com>
2020-10-30 23:51:56 +0530

    Added BCC

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import smtplib
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 # from paytmchecksum import Paytmchecksum
 import flask
 import requests
@@ -54,12 +55,15 @@ def test_server():
 #     return response
 
 
-def order_confirmation(args):
+@app.route("/order_request", methods=["POST"])
+def order_request():
+    args = request.get_json()
+
     message = EmailMessage()
 
     message["From"] = "orders.suneelprinters@gmail.com"
-    message["To"] = args["email"]
-    message["Subject"] = "Order Confirmation from Sunil Printers"
+    message["Bcc"] = ["orders.suneelprinters@gmail.com", f"{args['email']}"]
+    message["Subject"] = f"Order Placed by {args['name']}"
 
     message.add_header("Content-Type", "text/html")
     message.set_payload(
@@ -166,130 +170,10 @@ def order_confirmation(args):
     )
 
     mail_server = smtplib.SMTP_SSL("smtp.gmail.com")
+
     mail_server.login("orders.suneelprinters@gmail.com", "SuneelPrinters37")
     mail_server.send_message(message)
     mail_server.quit()
-
-    return "Successful"
-
-
-@app.route("/order_request", methods=["POST"])
-def order_request():
-    args = request.get_json()
-
-    message = EmailMessage()
-
-    message["From"] = "orders.suneelprinters@gmail.com"
-    message["To"] = "orders.suneelprinters@gmail.com"
-    message["Subject"] = f"Order Placed by {args['name']}"
-
-    message.add_header("Content-Type", "text/html")
-    message.set_payload(
-        f"""<!DOCTYPE html>
-        <html>
-        <head>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <style>
-                body {{
-                background-color: #F0F0F0;
-                font-family: sans-serif;
-                }}
-                #card {{
-                background-color: white;
-                padding: 4px 32px 32px 32px;
-                margin-top: 12px;
-                }}
-                .lefty {{
-                text-align: left;
-                }}
-                .righty {{
-                text-align: right;
-                }}
-                .center {{
-                display: block;
-                margin-left: auto;
-                margin-right: auto;
-                }}
-                table.product td, table.product th {{
-                padding-right: 10px;
-                padding-left: 10px;
-                padding-top: 6px;
-                padding-bottom: 6px;
-                }}
-                table.product th {{
-                border-bottom: 1px solid black;
-                }}
-                table {{
-                width: 100%;
-                border-collapse: collapse;
-                }}
-                table.product {{
-                margin-top: 18px;
-                }}
-                .fa {{
-                padding: 16px;
-                    font-size: 16px;
-                    width: 16px;
-                    text-align: center;
-                    text-decoration: none;
-                    margin: 5px 2px;
-                    border-radius: 50%;
-                }}
-                .fa-facebook {{
-                margin-top: 12px;
-                    background: #666;
-                    color: white;
-                }}
-            </style>
-        </head>
-        <body>
-        <img src="https://firebasestorage.googleapis.com/v0/b/suneelprinters37.appspot.com/o/Logo.png?alt=media&token=21acff59-dc39-411b-a881-f4dac1da5173", class="center" width="12%">
-        <div id="card">
-            <p style="font-size: 24px; font-weight: bold; text-align: center;">ORDER DETAILS</p>
-            <table>
-                <tr>
-                    <th class="lefty">Customer Name:</th>
-                    <td class="righty">{args['name']}</td>
-                </tr>
-                <tr>
-                    <th class="lefty">Phone Number:</th>
-                    <td class="righty">{args['phone']}</td>
-                </tr>
-                <tr>
-                    <th class="lefty">Payment Mode:</th>
-                    <td class="righty">{args['payment_mode']}</td>
-                </tr>
-                <tr>
-                    <th class="lefty">Shipping Address:</th>
-                    <td class="righty" width="50%">{args['address']}</td>
-                </tr>
-            </table>
-            <table class="product">
-                <tr>
-                    <th class="lefty">PRODUCT NAME</th>
-                    <th class="righty">QUANTITY</th>
-                    <th class="righty" width="40%">PRICE</th>
-                </tr>
-                    {args['product_list']}
-                <tr>
-                <th colspan="2" style="border-top: 1px solid black; text-align: left;">TOTAL:</th>
-                <th style="border-top: 1px solid black" class="righty">{args['price']}</th>
-                </tr>
-            </table>
-        </div>
-        <a href="http://www.facebook.com/" target="_blank">
-                <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" width="5%" class="center" />
-            </a>
-        </body>
-        </html>"""
-    )
-
-    mail_server = smtplib.SMTP_SSL("smtp.gmail.com")
-    mail_server.login("orders.suneelprinters@gmail.com", "SuneelPrinters37")
-    mail_server.send_message(message)
-    mail_server.quit()
-
-    order_confirmation(args)
 
     return "Successful"
 
