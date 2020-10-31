8dd851e902d52ba17baafe254becb23c3d7b856c
ljgoyal207 <67781472+ljgoyal207@users.noreply.github.com>
2020-10-31 19:46:40 +0530

    Added on order email

--- app.py
+++ app.py
@@ -173,6 +173,113 @@ def order_request():
     mail_server.quit()
 
     return "Successful"
+    
+@app.route("/onOrder")
+def onOrder():
+    args = request.args
+
+    message = EmailMessage()
+
+    message["From"] = "orders.suneelprinters@gmail.com"
+    message["Bcc"] = ["orders.suneelprinters@gmail.com", f"{args['email']}"]
+    message["Subject"] = f"Order Placed by {args['name']}"
+
+    message.add_header("Content-Type", "text/html")
+    message.set_payload(
+        f"""<!DOCTYPE html>
+        <html>
+        <head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <style>
+                body {{
+                background-color: #F0F0F0;
+                font-family: sans-serif;
+                }}
+                #card {{
+                background-color: white;
+                padding: 4px 32px 32px 32px;
+                margin-top: 12px;
+                }}
+                .lefty {{
+                text-align: left;
+                }}
+                .righty {{
+                text-align: right;
+                }}
+                .center {{
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                }}
+                table.product td, table.product th {{
+                padding-right: 10px;
+                padding-left: 10px;
+                padding-top: 6px;
+                padding-bottom: 6px;
+                }}
+                table.product th {{
+                border-bottom: 1px solid black;
+                }}
+                table {{
+                width: 100%;
+                border-collapse: collapse;
+                }}
+                table.product {{
+                margin-top: 18px;
+                }}
+                .fa {{
+                padding: 16px;
+                    font-size: 16px;
+                    width: 16px;
+                    text-align: center;
+                    text-decoration: none;
+                    margin: 5px 2px;
+                    border-radius: 50%;
+                }}
+                .fa-facebook {{
+                margin-top: 12px;
+                    background: #666;
+                    color: white;
+                }}
+            </style>
+        </head>
+        <body>
+        <img src="https://firebasestorage.googleapis.com/v0/b/suneelprinters37.appspot.com/o/Logo.png?alt=media&token=21acff59-dc39-411b-a881-f4dac1da5173", class="center" width="12%">
+        <div id="card">
+        <p style = 'font-size:12px; text-align : left;'>Hey {args['name']} <br> Greetings from Suneel Printers! <br><br> This is to confirm your order with Sunil Printers. </p>
+            <p style="font-size: 24px; font-weight: bold; text-align: center;">ORDER DETAILS</p>
+            <table>
+                <tr>
+                    <th class="lefty">Customer Name:</th>
+                    <td class="righty">{args['name']}</td>
+                </tr>
+                <tr>
+                    <th class="lefty">Phone Number:</th>
+                    <td class="righty">{args['phone']}</td>
+                </tr>
+               
+            </table>
+          <p style = font-size:18px; font-weight:bold;>ORDERS:</p>
+          <ol><li>{args['order_list']}</ol>
+                <p style = 'font-size:12px; text-align : center;'>You will soon recieve a call from Suneel Printers </p>
+                        <p style = 'font-size:18px; text-align : center;'> Thanks for Shopping with us!</p>
+        </div>
+        <a href="http://www.facebook.com/" target="_blank">
+                <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" width="5%" class="center" />
+            </a>
+        </body>
+        </html>"""
+    )
+
+    mail_server = smtplib.SMTP_SSL("smtp.gmail.com")
+
+    mail_server.login("orders.suneelprinters@gmail.com", "SuneelPrinters37")
+    mail_server.send_message(message)
+    mail_server.quit()
+    return 'Successful'
+
+
+
 
 
 if __name__ == "__main__":
